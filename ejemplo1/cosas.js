b064cb45bb8d695c5a0b082e8490977e4fef883f
xumaxa <56018430+xumaxa@users.noreply.github.com>
2022-09-20 09:12:09 +0200

    Update cosas.js

--- ejemplo1/cosas.js
+++ ejemplo1/cosas.js
@@ -3,3 +3,11 @@ tabla = prompt("indique tabla");
 for(var i=1;i<=10;i++){
     document.write(i + " * " + tabla + " = " + i*tabla + "<br>");
 }
+
+function suma(){
+    document.write(5+3);
+   
+}
+function resta(){
+    document.write(5-3);
+}
