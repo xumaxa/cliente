8a2975ea22e2085c79276e273fea4c2053a10592
xumaxa <56018430+xumaxa@users.noreply.github.com>
2022-09-22 09:08:37 +0200

    Update ejemplo.js

--- ejemplo6/ejemplo.js
+++ ejemplo6/ejemplo.js
@@ -1,10 +1,10 @@
 var palabra = prompt("Indroduzca una palabra para saber cual es la letra que más se repite");
 
 var array1 = palabra.split("");
-var letras = [];
-var numeros=[];
-
-for(i in array1){
+/*var letras = [];
+var numeros=[];*/
+const dict = new Dictionary();
+/*for(i in array1){
     var letra = array1[i];
     for(i in letras){
         if(letra!=letras[i]){
@@ -14,11 +14,16 @@ for(i in array1){
         else{
             for(i in letras[i]){
                 if(letras[i]==letra){
-                    
+                
                 }
             }
         }
     }
     
    
+}*/
+for(i in array1){
+    if(!(dict.has("\"" +array1[i] +"\""))){
+
+    }
 }
