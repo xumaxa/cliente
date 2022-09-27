5ae4a4ad839ed0b1a956fd96273b40db90c1e508
xumaxa <56018430+xumaxa@users.noreply.github.com>
2022-09-27 09:59:45 +0200

    Create ejemplo.js

---
+++ ejercicio8/ejemplo.js
@@ -0,0 +1,18 @@
+var boton = document.getElementById("boton");
+
+boton.onclick = function() {
+    provincia= prompt("Escribe una provincia para añadirla a la lista");
+    listaProvincia = document.createElement("li");
+    provinciaNode=document.createTextNode(provincia);
+    listaProvincia.appendChild(provinciaNode);
+    document.getElementById("lista").appendChild(listaProvincia);
+}
+
+var boton2 = document.getElementById("eliminar");
+
+boton2.onclick = function() {
+    lista = document.getElementsByTagName("li");
+    ultimaPosicion = lista.length-1;
+    lista[ultimaPosicion].parentNode().removeChild(lista[ultimaPosicion]);
+
+}
